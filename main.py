bb260d726203bd285d996282e77ae396b84703f3
mname <uche.chinedu@e4email.net>
2021-11-11 13:02:41 +0100

    add static files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #main.py
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.generalpages.route_homepage import general_pages_router
+
 
 
 
@@ -10,9 +12,13 @@ def include_router(app):
     app.include_router(general_pages_router)
 
 
+def configure_static(app):  #new
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app
 
 app = start_application()
