5a1984b3b38cf2a4977a2287cdc865f31f9d6d9f
mname <uche.chinedu@e4email.net>
2021-11-11 14:00:04 +0100

    create database tables

---
+++ db/base_class.py
@@ -0,0 +1,2 @@
+from typing import Any
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.generalpages.route_homepage import general_pages_router
+from db.session import engine
+from db.base_class import Base  
 
 
 
@@ -12,13 +14,17 @@ def include_router(app):
     app.include_router(general_pages_router)
 
 
-def configure_static(app):  #new
+def configure_static(app): 
     app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
     configure_static(app)
+    create_tables()
     return app
 
 app = start_application()
